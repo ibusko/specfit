7c7987affc8e8eb3ac18f359b9bbe5b1f3d0bc1c
ibusko <busko@stsci.edu>
2015-04-07 16:04:36 -0400

    Gaussian fit results

--- proto/fit_demo.py
+++ proto/fit_demo.py
@@ -3,10 +3,15 @@ import sys
 import fit_functions
 
 
-data = {'n5548':  ["../data/n5548/",  "n5548_mean_g130mb4.asc",  "n5548_lyalpha_sample.dat", "sfn5548_lyalpha2"],
-        'obs1g_1':["../data/simple/", "obs1g.dat",  None, "obs1g_model_1.dat"],
-        'obs1g_2':["../data/simple/", "obs1g.dat",  None, "obs1g_model_2.dat"],
-        'obs1gn': ["../data/simple/", "obs1gn.dat", None, "obs1g_model_2.dat"]
+data = {'n5548':   ["../data/n5548/",  "n5548_mean_g130mb4.asc",  "n5548_lyalpha_sample.dat", "sfn5548_lyalpha2"],
+        'obs1g_1': ["../data/simple/", "obs1g.dat",  None, "obs1g_model_1.dat"],
+        'obs1g_2': ["../data/simple/", "obs1g.dat",  None, "obs1g_model_2.dat"],
+        'obs1gn_1':["../data/simple/", "obs1gn.dat", None, "obs1g_model_1.dat"],
+        'obs1gn_2':["../data/simple/", "obs1gn.dat", None, "obs1g_model_2.dat"],
+        'obs2g_1': ["../data/simple/", "obs2g.dat",  None, "obs2g_model_1.dat"],
+        'obs2g_2': ["../data/simple/", "obs2g.dat",  None, "obs2g_model_2.dat"],
+        'obs2gn_1':["../data/simple/", "obs2gn.dat", None, "obs2g_model_1.dat"],
+        'obs2gn_2':["../data/simple/", "obs2gn.dat", None, "obs2g_model_2.dat"],
 
 }
 
